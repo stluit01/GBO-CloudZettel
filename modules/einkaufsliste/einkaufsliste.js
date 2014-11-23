5ba2fb7b24ac438be4035ddf1b6813ecf198fb40
jens1105 <jens1105@de-schmid.de>
2014-11-23 21:28:51 +0100

    add Article / fix id bug user

--- modules/einkaufsliste/einkaufsliste.js
+++ modules/einkaufsliste/einkaufsliste.js
@@ -21,7 +21,9 @@ angular.module('einkaufsliste', ['einkaufsliste.config'])
 
     .controller('editListeCtrl', function ($scope, storage, $routeParams, $location) {
         'use strict';
-        $scope.isCollapsedHead = true;
+
+        $scope.showNew = false;
+        $scope.isCollapsed = true;
         $scope.isCollapsed = true;
 
         $scope.back = function () {
@@ -51,27 +53,60 @@ angular.module('einkaufsliste', ['einkaufsliste.config'])
             $scope.isCollapsedHead = true;
         }
 
-        $scope.plus = function (name) {
+        $scope.plus = function (id) {
+            //TODO REST!!
             for (var i = 0, n = $scope.liste.article.length; i < n; i++) {
-                if (name === $scope.liste.article[i].name) {
+                if (parseInt(id) === $scope.liste.article[i].id) {
                     $scope.liste.article[i].count++
                 }
             }
         }
 
-        $scope.minus = function (name) {
+        $scope.minus = function (id) {
+            //TODO REST!!
             for (var i = 0, n = $scope.liste.article.length; i < n; i++) {
-                if (name === $scope.liste.article[i].name) {
+                if (parseInt(id) === $scope.liste.article[i].id) {
                     $scope.liste.article[i].count--
                 }
             }
         }
-        $scope.remArticle = function (item) {
-            storage.remArticle(article);
+        $scope.remArticle = function (id) {
+            //storage.remArticle(id);
+            var i =  $scope.liste.article.length;;
+            while (i--) {
+                if (parseInt(id) === $scope.liste.article[i].id) {
+                    $scope.liste.article.splice(i, 1);
+                }
+            }
         }
 
         $scope.updateListe = function () {
             $scope.isCollapsedHead = true
             storage.updateListe( $scope.liste);
         }
+
+        $scope.newArtikelName = "";
+        $scope.newArtikelCount = 1;
+
+        $scope.newArtikel = function (name,count) {
+            //TODO ID !!!!! //TODO REST
+            $scope.showNew = false;
+            var newArticle =
+            {
+                "id" : 500,
+                "name" : name,
+                "count" : count,
+                "purchased" : false
+            };
+            $scope.newArtikelName = "";
+            $scope.newArtikelCount = 1;
+            $scope.liste.article.push(newArticle);
+        }
+
+        $scope.abortNewArtikel = function (name,count) {
+            $scope.showNew = false;
+            $scope.newArtikelName = "";
+            $scope.newArtikelCount =1;
+        }
+
     });
