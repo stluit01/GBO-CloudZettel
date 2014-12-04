34b317bcb9b474b13c5645f510e68855018493e6
Stefan Lutz <stluit01@hs-esslingen.de>
2014-12-04 17:20:30 +0100

    Implemented confirm dialog for deleting a list in the overview of a lists by clicking on the cross icon

--- modules/einkaufsliste/controller/listsCtrl.js
+++ modules/einkaufsliste/controller/listsCtrl.js
@@ -4,25 +4,42 @@
 einkaufsliste.controller('listsCtrl', function ($scope, listsDataService, $location) {
     'use strict';
 
+    $scope.showConfirmDialog = false;
+    var idToBeDeleted = -1;
+
     listsDataService.getLists().then(function (res) {
         $scope.lists = res.data;
     });
 
     $scope.remListe = function (id) {
-        listsDataService.removeList(id).then(function () {
-            for(var i=0; i < $scope.lists.length; i++){
-                //console.log($scope.lists[i].id +" : "+ id);
-                if($scope.lists[i].id === id){
-                    $scope.lists.splice(i, 1);
-                    break;
+        idToBeDeleted = id;
+        $scope.showConfirmDialog = true;
+    };
+
+    $scope.deleteListConfirmed = function(){
+        if(idToBeDeleted > 0){
+            listsDataService.removeList(idToBeDeleted).then(function () {
+                for(var i=0; i < $scope.lists.length; i++){
+                    //console.log($scope.lists[i].id +" : "+ id);
+                    if($scope.lists[i].id === idToBeDeleted){
+                        $scope.lists.splice(i, 1);
+                        break;
+                    }
                 }
-            }
-        });
-    }
+                idToBeDeleted = -1;
+            });
+        }
+        $scope.showConfirmDialog = false;
+    };
+
+    $scope.deleteListDeclined = function(){
+        idToBeDeleted = -1;
+        $scope.showConfirmDialog = false;
+    };
 
     $scope.edit = function (id) {
         $location.search('id', id);
         $location.path('/addEditList');
-    }
+    };
 
-})
+});
